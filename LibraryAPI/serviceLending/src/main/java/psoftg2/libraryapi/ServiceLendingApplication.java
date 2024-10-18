6ff757b92b786228ce15916a727433dad22c0ee9
Catarina <1221348@isep.ipp.pt>
2024-10-18 23:07:35 +0100

    Código: Separação do código em serviços independentes 8081: AuthManager 8082: BookManager 8083: LendingManager 8084: ReaderManager
    !!! Não comunicam entre si !!!

---
+++ LibraryAPI/serviceLending/src/main/java/psoftg2/libraryapi/ServiceLendingApplication.java
@@ -0,0 +1,12 @@
+package psoftg2.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLendingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLendingApplication.class, args);
+    }
+}
